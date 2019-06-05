1f1f9f91458e6b4ccc53fec12108cd1691a4f3c2
zfletch <zfletch2@gmail.com>
2019-06-05 14:47:29 -0400

    write tests

---
+++ src/components/Page/Page.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import { PerseidsFooter } from 'perseids-react-components';
+
+import Home from '../Home';
+import PublicationDirector from '../PublicationDirector';
+import PublicationGroupDirector from '../PublicationGroupDirector';
+
+const Page = ({
+  config,
+  config: {
+    copyright,
+    report,
+    github,
+    twitter,
+  },
+}) => (
+  <>
+    <Switch>
+      <Route exact path="/" component={() => <Home config={config} />} />
+      <Route exact path="/:publication/" render={props => <PublicationGroupDirector {...props} config={config} />} />
+      <Route exact path="/:publication/:chunk" render={props => <PublicationDirector {...props} config={config} />} />
+    </Switch>
+    <PerseidsFooter
+      copyright={copyright}
+      report={report}
+      github={github}
+      twitter={twitter}
+    />
+  </>
+);
+
+export default Page;
