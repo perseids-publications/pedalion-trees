7b93fea17ed00546b286e32b834891779d2ed375
zfletch <zfletch2@gmail.com>
2019-06-07 15:27:13 -0400

    Updates

---
+++ precache-manifest.98223b014eb085b5a8c2ae70982ba1dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8b1f9b9132d5bfb9edb841e7dfa8e03",
+    "url": "/pedalion-trees/index.html"
+  },
+  {
+    "revision": "655c614efbbaf9367eaf",
+    "url": "/pedalion-trees/static/css/2.ec3f0427.chunk.css"
+  },
+  {
+    "revision": "41fff374eac9b48a01b3",
+    "url": "/pedalion-trees/static/css/main.a730fb68.chunk.css"
+  },
+  {
+    "revision": "655c614efbbaf9367eaf",
+    "url": "/pedalion-trees/static/js/2.21d3e77f.chunk.js"
+  },
+  {
+    "revision": "41fff374eac9b48a01b3",
+    "url": "/pedalion-trees/static/js/main.83569010.chunk.js"
+  },
+  {
+    "revision": "368900de7b6857193b35",
+    "url": "/pedalion-trees/static/js/runtime~main.24268fea.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pedalion-trees/precache-manifest.19148b7015b0b53a1676c6a47e4243f3.js"
+  "/pedalion-trees/precache-manifest.98223b014eb085b5a8c2ae70982ba1dc.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/pedalion-trees/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pedalion-trees/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
