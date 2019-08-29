91d08edd6a82b82bddf90ff9e452f83e83a385d0
Toon Van Hal <u0041239@kuleuven.be>
2019-08-29 16:46:21 +0200

    Updates

---
+++ precache-manifest.3d3da72d783d20dd5dbb0217e0df39d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc63198aeb48a8cba3ab93eceaeb6825",
+    "url": "/pedalion-trees/index.html"
+  },
+  {
+    "revision": "b8ac5c70ed7a5178f856",
+    "url": "/pedalion-trees/static/css/2.bc649c47.chunk.css"
+  },
+  {
+    "revision": "fbca79d39b3543c1ff2f",
+    "url": "/pedalion-trees/static/css/main.eb56760c.chunk.css"
+  },
+  {
+    "revision": "b8ac5c70ed7a5178f856",
+    "url": "/pedalion-trees/static/js/2.748eae5d.chunk.js"
+  },
+  {
+    "revision": "fbca79d39b3543c1ff2f",
+    "url": "/pedalion-trees/static/js/main.7de51ceb.chunk.js"
+  },
+  {
+    "revision": "368900de7b6857193b35",
+    "url": "/pedalion-trees/static/js/runtime~main.24268fea.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pedalion-trees/precache-manifest.ae04731cbedee4a35c1973e1eb735b77.js"
+  "/pedalion-trees/precache-manifest.3d3da72d783d20dd5dbb0217e0df39d4.js"
 );
 
 self.addEventListener('message', (event) => {
